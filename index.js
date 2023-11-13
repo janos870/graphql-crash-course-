8d5c1a632c7559fa71dcdfb299c459015422dc54
janos870 <janos.balogh870@gmail.com>
2023-11-13 21:10:39 +0100

    create a graphql server

---
+++ index.js
@@ -0,0 +1,87 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// data
+import db from "./_db.js";
+
+// types
+import { typeDefs } from "./schema.js";
+
+// resolvers
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+    authors() {
+      return db.authors;
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+    reviews() {
+      return db.reviews;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.author_id === parent.id);
+    },
+  },
+  Review: {
+    game(parent) {
+      return db.games.find((g) => g.id === parent.game_id);
+    },
+    author(parent) {
+      return db.authors.find((a) => a.id === parent.author_id);
+    },
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      return db.games.filter((g) => g.id !== args.id);
+    },
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+      db.games.push(game);
+      return game;
+    },
+    updateGame(_, args) {
+      db.games = db.games.map((game) => {
+        if (game.id === args.id) {
+          return {
+            ...game,
+            ...args.edits,
+          };
+        }
+        return game;
+      });
+      return db.games.find((game) => game.id === args.id);
+    },
+  },
+};
+
+// server setup
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server ready at: ${url}`);
